f0ce208e5860fe6a01acecb437ff50c3b0a7e495
Jason Balamb <jasonst.106@gmail.com>
2023-05-13 10:29:08 -0500

    adding a 'filer' method with a bolean example

--- server/04-filter.js
+++ server/04-filter.js
@@ -17,3 +17,31 @@ console.log('original', words);
 const ans = words.filter(item => item.length >= 6)
 console.log('ans', ans);
 console.log('original', words);
+
+//an application case of 'filter' with a boolean
+
+const orders = [
+    {
+        customerName: 'Nicolas',
+        total: 60,
+        delivered: true,
+    },
+    {
+        customerName: 'Zulema',
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: 'Santiago',
+        total: 180,
+        delivered: true,
+    },
+    {
+        customerName:'Taylor',
+        total: 240,
+        delivered: true,
+    },
+];
+
+const ans2 = orders.filter(item => item.delivered && item.total >= 100);
+console.log('ans2', ans2);
