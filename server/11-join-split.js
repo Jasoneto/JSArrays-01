05130c47b46e03038fea49877ab3d66d0136fcf7
Jason Balamb <jasonst.106@gmail.com>
2023-05-16 20:50:36 -0500

    adding the use of '.join' and 'split' with an Url example

---
+++ server/11-join-split.js
@@ -0,0 +1,19 @@
+const elements = ['fire', 'Air', 'water'];
+
+//EXAMPLE 1
+let finalAns = '';
+const separator = '--';
+for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
+    finalAns = finalAns + element + separator;
+}
+
+const ans = elements.join('--');
+console.log('for', finalAns);
+console.log('join', ans);
+
+//EXAMPLE 2 with 'split':
+const title = 'Arrays manipulation course';
+
+const finalUrl = title.split(' ').join('-').toLowerCase(); //if I don't put a space into the '.split(' '), it separate letter to letter LOL
+console.log(finalUrl);
