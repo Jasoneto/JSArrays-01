538c71b34d0e2016f1b835de92fc417967da59c4
Jason Balamb <jasonst.106@gmail.com>
2023-05-18 09:45:48 -0500

    a line code deleted in 01-for-each.js folder

--- server/01-for-each.js
+++ server/01-for-each.js
@@ -8,6 +8,3 @@ for (let index = 0; index < letters.length; index++) {
 //the same thing above but in one line
 letters.forEach(item => console.log('forEach' ,item))
 
-//This is the basic structure of iteration with arrays
-
-array.forEach(function(element, index, array))
